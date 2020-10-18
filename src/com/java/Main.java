706851c6ac49cbb22b0b774aa2eaa3ecc964060f
Chandan Kumar <ChandanKumar@signiwis.com>
2020-10-18 13:10:41 +0530

    Create Persion Class Git commit with Java Project

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -6,5 +6,36 @@ public class Main {
 	// write your code here
 
         System.out.println("This is my First Git Project with Intellij");
+
+    }
+}
+class Persion{
+
+    private String name;
+    private int age;
+    private  int id;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
