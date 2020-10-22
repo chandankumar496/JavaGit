a887cc00572628464307345c7056a3e63c42d4b5
Chandan Kumar <ChandanKumar@signiwis.com>
2020-10-22 13:53:33 +0530

    Filter data from Persion Git commit with Java Project

--- src/com/java/GitMerge.java
+++ src/com/java/GitMerge.java
@@ -4,7 +4,7 @@ public class GitMerge {
 
     private String m1 = "m1";
     private String m2 = "";
-    private String m3 = "";
+    private String m3 = "m4";
 
     public static void main(String[] args) {
 
