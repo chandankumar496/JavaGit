a41942f4598eaf6266ad27559facca907af06a60
Chandan Kumar <ChandanKumar@signiwis.com>
2020-10-22 14:02:07 +0530

    3

--- src/com/java/GitMerge.java
+++ src/com/java/GitMerge.java
@@ -3,7 +3,7 @@ package com.java;
 public class GitMerge {
 
     private String m1="m1";
-    private String name = "fm2";
+    private String name = "fm3";
 
     public static void main(String[] args) {
 
