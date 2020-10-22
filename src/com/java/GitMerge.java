30ff9d31d90c2f9a91048d45e92d1d190765807b
Chandan Kumar <ChandanKumar@signiwis.com>
2020-10-22 12:41:24 +0530

    m1

---
+++ src/com/java/GitMerge.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class GitMerge {
+
+    private String m1 = "m1";
+    private String m2 = "";
+    private String m3 = "";
+
+    public static void main(String[] args) {
+
+    }
+}
